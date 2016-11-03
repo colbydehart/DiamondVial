7aaf8fa0fe3266e157a990b3eebef442d0fc018f
Colby DeHart <colbydehart@gmail.com>
2016-11-03 12:09:14 -0400

    Cleaned up indentation

--- lib/diamond_vial.ex
+++ lib/diamond_vial.ex
@@ -1,16 +1,16 @@
 defmodule DiamondVial do
-	defmacro input <~> expression do
+  defmacro input <~> expression do
     {function, context, args} = expression
     new_args = swap_args args, input
     {function, context, new_args}
-	end
+  end
 
   def swap_args(args, input) do
     Enum.map(args, fn arg ->
       case arg do
         {func, con, args} -> {func, con, swap_args(args, input)}
-        :it -> input
-        a -> a
+        :it               -> input
+        _                 -> arg
       end
     end)
   end
