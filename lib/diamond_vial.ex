197434867e7a8507a43295609d9f900e1c9f5797
Colby DeHart <colbydehart@gmail.com>
2016-11-02 20:35:40 -0400

    we did it

--- lib/diamond_vial.ex
+++ lib/diamond_vial.ex
@@ -1,7 +1,19 @@
 defmodule DiamondVial do
 	defmacro input <~> expression do
-		quote do
-			unquote(input) |> unquote(expression)
-		end
+    {function, context, args} = expression
+    new_args = swap_args args, input
+    {function, context, new_args}
 	end
+
+  def swap_args(args, input) do
+    Enum.map(args, fn arg ->
+      case arg do
+        {func, con, args} -> {func, con, swap_args(args, input)}
+        :it -> input
+        a -> a
+      end
+    end)
+  end
 end
+
+
--- test/diamond_vial_test.exs
+++ test/diamond_vial_test.exs
@@ -2,13 +2,23 @@ defmodule DiamondVialTest do
   use ExUnit.Case
 	import DiamondVial
 
-  test "Can pipe an input into an expression" do
-    assert [1, 2, 3] <~> Enum.sum == 6
+  test "Can pipe an arbitrary position" do
+    res = 2 <~> 3 + :it
+    assert res == 5
   end
 
-  # test "Can pipe an arbitrary position" do
-	# 	hero = %{name: %{first: "Han", last: "Solo"}}
-	# 	villain = [:name, :first] <~> put_in(hero, :it, "Ben") 
-  #   assert villain == %{name: %{first: "Ben", last: "Solo"}}
-  # end
+  test "Can pipe an arbitrary position with more than one func" do
+    res = 2 <~> 3 + 4 + :it
+    assert res == 9
+  end
+
+  test "Can pipe with funky funcs" do
+    res = 2 <~> :it + 3 * :it
+    assert res == 8
+  end
+
+  test "Can pipe with more funky funcs" do
+    res = 2 <~> :it + 3 <~> :it * 4 <~> :it + :it
+    assert res == 40 
+  end
 end
